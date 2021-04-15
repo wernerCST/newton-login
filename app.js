32fec0718f880dc68bd3e260b89e6194f7967a6b
Werner <wernercastanaza@gmail.com>
2021-04-15 12:46:29 -0400

    move routes around as well as added connection to mongodb in my local machine

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 const express = require('express');
 
 const userRouter = require('./routes/userRouter');
-
 const app = express();
 
 app.use('/api/v1/users', userRouter);
 
 
-
-
-const port = 3000;
-app.listen(port, () => {
-    console.log(`App running on port: ${port}`);
-});
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE_LOCAL
+mongoose
+.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+.then(() => console.log('DB connection succesful'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App running on port: ${port}`);
+});
